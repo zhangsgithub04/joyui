3906168d8c42ad9507263d9bc1127eb2443303d8
zhangsgithub04 <ssen_zhang@hotmail.com>
2024-01-26 17:59:16 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
+import Box from '@mui/joy/Box';
 import Demo from './Demo';
 import Demo2 from './Demo2';
 import DemoAutoComplete from './DemoAutoComplete'; 
